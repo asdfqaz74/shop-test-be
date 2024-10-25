a9df501b5892e16777f5a28dbcb12512f0a46fa2
KangJS <asdfqaz1996@naver.com>
2024-10-26 00:07:42 +0900

    Refactor user.controller.js: Update password hashing and add success response

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,21 +7,23 @@ userController.createUser = async (req, res) => {
   try {
     // 비구조화 할당을 통해 email, password, name을 req.body에서 추출합니다.
     const { email, password, name, level } = req.body;
+    console.log(req.body);
 
     // User 컬렉션에서 email이 일치하는 document를 찾습니다.
     const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ error: "이미 존재하는 이메일입니다." });
     }
+    console.log(req.body);
 
     // 비밀번호를 암호화합니다.
     const salt = await bcrypt.genSaltSync(10);
-    password = await bcrypt.hash(password, salt);
+    const hasedPassword = await bcrypt.hashSync(password, salt);
 
     // User 컬렉션에 document를 생성합니다.
     const newUser = new User({
       email,
-      password,
+      password: hasedPassword,
       name,
       level: level ? level : "customer",
     });
@@ -29,7 +31,9 @@ userController.createUser = async (req, res) => {
     // document를 저장합니다.
     await newUser.save();
 
-    return res.status(200).send("회원가입이 완료되었습니다.");
+    return res
+      .status(200)
+      .json({ status: "success", message: "회원가입에 성공했습니다." });
   } catch (e) {
     res.status(400).json({ error: e });
   }
