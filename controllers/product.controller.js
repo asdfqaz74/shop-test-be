dec5309d91a3ea63f1fd953247cc97518ba85439
KangJS <asdfqaz1996@naver.com>
2024-10-31 00:36:44 +0900

    Refactor product.controller.js: Add getProducts method and update product.api.js routes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -34,4 +34,13 @@ productController.createProduct = async (req, res) => {
   }
 };
 
+productController.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ status: "success", products });
+  } catch (e) {
+    res.status(500).json({ error: e.message || "서버 오류" });
+  }
+};
+
 module.exports = productController;
